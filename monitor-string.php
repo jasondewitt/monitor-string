aba4366b0f1d72044fa4e00fc4d49b48d088b80c
Jason <me@roxet.net>
2016-11-27 14:26:46 -0600

    keeping it simple stupid
    got some good feedback from my buddy @stevegrunwell and I've simplied this a lot

--- monitor-string.php
+++ monitor-string.php
@@ -25,51 +25,89 @@
  * Domain Path:       /languages
  */
 
+namespace Monitor_String;
+
+define('PLUGIN_NAME', 'monitor-string');
+
+
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-monitor-string-activator.php
- */
-function activate_monitor_string() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-monitor-string-activator.php';
-	Monitor_String_Activator::activate();
+# add admin setting page
+function plugin_menu() {
+  add_options_page( 'String Monitor Setup', 'String Monitor', 'manage_options', PLUGIN_NAME, __NAMESPACE__ . '\options_page' );
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-monitor-string-deactivator.php
- */
-function deactivate_monitor_string() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-monitor-string-deactivator.php';
-	Monitor_String_Deactivator::deactivate();
+add_action( 'admin_menu', __NAMESPACE__ . '\plugin_menu');
+
+function options_page() {
+  ?>
+  <script type="text/javascript">
+    function randomString(length) {
+        var result = '';
+        var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+        return result;
+    }
+  </script>
+  <div class="wrap">
+
+      <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+      <form method="post" name="monitor_string_options" action="options.php">
+
+        <?php
+          //Grab all options
+          $options = get_option(PLUGIN_NAME);
+
+          $string_to_monitor = $options['string'];
+          settings_fields(PLUGIN_NAME);
+          do_settings_sections(PLUGIN_NAME);
+        ?>
+      <p>String to check for: <input type="text" id=<?php echo PLUGIN_NAME?>-string name="<?php echo PLUGIN_NAME?>[string]" value="<?php echo $string_to_monitor?>" class="regular-text" /><br /><br />
+        <input class="button-secondary" type="button" onclick='jQuery("#monitor-string-string").val(randomString(32));' value="<?php esc_attr_e( 'Generate Random String' ); ?>" />
+      <?php submit_button('Save', 'primary','submit', false); ?>
+
+     </form>
+
+  </div>
+
+  <?php
 }
 
-register_activation_hook( __FILE__, 'activate_monitor_string' );
-register_deactivation_hook( __FILE__, 'deactivate_monitor_string' );
+function settings_init() {
+   register_setting(PLUGIN_NAME, PLUGIN_NAME, __NAMESPACE__ . '\validate' );
+}
+add_action( 'admin_init', __NAMESPACE__ . '\settings_init' );
 
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-monitor-string.php';
+function validate($input) {
+    // All checkboxes inputs
+    $valid = array();
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_monitor_string() {
+    if (isset($input['string']) && !empty($input['string'])) {
+      $valid['string'] = sanitize_text_field($input['string']);
+    } else {
+      add_settings_error(
+        'monitor_string_invalid',
+        'monitor_string_empty',
+        'please enter a string',
+        'error'
+      );
+    }
 
-	$plugin = new Monitor_String();
-	$plugin->run();
+    return $valid;
 
 }
-run_monitor_string();
+
+function render_monitor_string() {
+	$string = get_option( PLUGIN_NAME, false )['string'];
+
+	if ( ! $string ) {
+		return;
+	}
+
+	printf( "<!-- Monitor-String %s -->", esc_html( $string ) );
+}
+add_action( 'wp_footer', __NAMESPACE__ . '\render_monitor_string', 1000 );
